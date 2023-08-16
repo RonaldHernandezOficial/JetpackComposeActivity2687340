a9d6fb16b1380295d9d9fc2122283310ddd1961f
Your Name <you@example.com>
2023-08-16 16:26:09 -0500

    Subida Proyecto Jetpack Compose

---
+++ app/src/main/java/co/edu/sena/jetpackcomposeactivity2687340/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package co.edu.sena.jetpackcomposeactivity2687340.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)40 = Color(0xFF7D5260)
+
+private val LightColorScheme = lightColorScheme(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
